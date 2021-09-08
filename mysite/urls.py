8d3a18171acdc64a14bd7ae76bf68c462663a54d
arefinncode <arefin.nordecode@gmail.com>
2021-09-08 08:50:11 +0600

    post list in blog---- url blog/

--- mysite/urls.py
+++ mysite/urls.py
@@ -83,6 +83,8 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    # path('', include('blog.urls')),
 ]
 
 
