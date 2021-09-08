f85e41abbfd6b0997468ef2e489bc9a2a5459ebc
arefinncode <arefin.nordecode@gmail.com>
2021-09-08 11:38:55 +0600

    commit for remote pc urls.py updated at 11:38 am

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,17 @@
 
 
 from django.urls import path
+from django.conf.urls import url, include
 from . import views
 
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('upload/', views.post_list, name='UserAvatarUpload'),
+    # path('', views.post_list, name='post_list'),
+    path('upload/', views.UserAvatarUpload, name='UserAvatarUpload'),
+
+    # url(r'^$', views.index, name='index'),
+    # url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    # url(r'^post/new/$', views.post_new, name='post_new'),
+    # url(r'^user/login/$', views.login, name='login'),
 
 ]
